12c2e49c72e641fe42c17e4f849b61a3e94034c2
Michael Liao <askxuefeng@gmail.com>
2016-11-25 18:29:07 +0800

    update tushare

--- pyquant/vendors/tushare.py
+++ pyquant/vendors/tushare.py
@@ -12,17 +12,29 @@ __author__ = 'Michael Liao'
 import logging
 import tushare
 
+from datetime import date
+
 from pyquant import configs
 from pyquant.models import Vendor
 
-NAME = 'tushare'
+class DataVendor(object):
 
-def init():
-    vs = Vendor.findAll(where='name=?', args=(NAME,))
-    if len(vs)==0:
-        v = Vendor(name=NAME, url='http://tushare.org')
-        v.save()
+    def __init__(self):
+        self.name = 'tushare'
+        self.since = '2001-01-01'
+        vs = Vendor.findAll(where='name=?', args=(self.name,))
+        if len(vs)==0:
+            v = Vendor(name=self.name, url='http://tushare.org')
+            v.save()
         logging.info('Init tushare ok.')
 
-def update(since=None):
-    pass
+    def update(self):
+        '''
+        Called by data-fetch thread.
+        '''
+        pass
+
+    def query(self, symbol, tickType, fromTime, endTime):
+        pass
+
+
